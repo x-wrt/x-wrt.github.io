27038642b92975a242d6546c393e3dccbf709135
ruqli <ruqli@microsoft.com>
2019-07-16 22:03:02 +0800

    Add tutorial newifi3

---
+++ docs/tutorial/newifi3/README.md
@@ -0,0 +1,21 @@
+# 新路由3刷机教程
+
+## 一键刷Breed
+推荐使用[一键刷Breed程序](https://www.right.com.cn/forum/forum.php?mod=viewthread&tid=343058&page=1#pid3139054)。
+### macOS
+打开终端，执行：
+```sh
+git clone https://github.com/x-wrt/x-wrt.github.io
+cd x-wrt.github.io/docs/tutorial/newifi3
+pip3 install -r requirements.txt
+python3 zy_y.py
+```
+### Windows
+如果会装 Python，建议使用类似 macOS 步骤。否则，下载[压缩包](NEWIFI3.exe.zip)，解压缩双击。
+
+成功后路由器会自动重启。断电后按<code>复位健</code>或者<code>USB键</code>开机均可进入 Breed。
+
+## 刷入 X-Wrt 固件
+可以下载本文作者测试过的[固件](x-wrt-5.0-b201907141234-ramips-mt7621-d-team_newifi-d2-squashfs-sysupgrade.bin) 或者在 https://dl.x-wrt.net/rom/ 搜索 <code>newifi-d2-squashfs-sysupgrade.bin<code> 下载最新固件。
+
+用网线连接路由器，浏览器打开 http://192.168.1.1 进 Breed 控制台。点固件更新，上传固件。闪存布局不用改，点击上传，然后确认。3分钟之后可以搜到 X-WRT 开头的热点。
---
+++ docs/tutorial/newifi3/zy_y.py
@@ -0,0 +1,144 @@
+#-*- coding:utf8 -*-
+import paramiko
+import urllib.request
+import time
+import urllib
+import requests
+import webbrowser
+import socket
+
+
+#变量配置
+down ="./eeprom.bin"
+router = "http://192.168.99.1"
+eeprom = "http://192.168.99.1/eeprom.bin"
+openssh = "http://192.168.99.1/newifi/ifiwen_hss.html"
+#open = "http://192.168.99.1"
+
+print()
+print("NEWIFI3 一键刷入Breed")
+print()
+print("CopyRight By Zy143L")
+print("Breed CopyRight Hackpascal")
+print()
+print("正在侦测路由器")
+print()
+
+try:
+    code = requests.get(openssh, timeout=5).status_code
+    #print(r)
+    #code = r
+except requests.exceptions.ConnectTimeout:
+    print("无法侦测到路由器"),print("请检查路由器状态")
+    time.sleep(1)
+    print("回车退出程序")
+    input()
+    exit()
+
+
+def shizuku ():
+    while True:
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname="192.168.99.1", port=22, username="root", password=input("路由器密码: "))
+            break
+        except paramiko.ssh_exception.AuthenticationException:
+            print("路由器密码错误 请重试"), print()
+            shizuku()
+    # ssh.connect(hostname="127.0.0.1",username="shizuku",password="shizuku")
+    print(), print("路由器连接成功")
+    stdin, stdout, stderr = ssh.exec_command("wget https://gitee.com/zy143l/OpenWRT/raw/master/zy")
+    result = stdout.read()
+    stdin, stdout, stderr = ssh.exec_command("wget https://gitee.com/zy143l/OpenWRT/raw/master/shizuku.ko")
+    result = stdout.read()
+    stdin, stdout, stderr = ssh.exec_command("sh zy")
+    result = stdout.read()
+    # ssh.exec_command("wget https://gitee.com/zy143l/OpenWRT/raw/master/zy")
+    # ssh.exec_command("wget https://gitee.com/zy143l/OpenWRT/raw/master/shizuku.ko")
+    # ssh.exec_command("sh zy")
+    print()
+    print("EEPROM备份成功 开始下载")
+    urllib.request.urlretrieve(eeprom, down)
+    time.sleep(1)
+    print()
+    print("下载完成: "+down)
+    print()
+    print("回车开始刷机")
+    input()
+    stdin, stdout, stderr = ssh.exec_command("insmod shizuku.ko")
+    result = stdout.read()
+    #ssh.close()
+    print(), print("成功")
+    print("CopyRight By Zy143L")
+    time.sleep(5)
+    exit()
+
+    # stdin,stdout,stderr = ssh.exec_command("wget -P /tmp https://gitee.com/zy143l/OpenWRT/raw/master/shizuku.ko")
+    # result = stdout.read()
+    # if not result:
+    # result = stderr.read()
+
+    # print(result.decode())
+    #return
+
+
+if code == 404:
+    time.sleep(1)
+    print("路由器配置异常!"),print("请在浏览器打开的配置路由器"),print("即将通过浏览器打开配置页面"),print("请配置好路由器后再次打开本程序")
+    print("回车确定")
+    input()
+    #webbrowser.open(router, new=0, autoraise=True)
+    webbrowser.open_new(router)
+    #webbrowser.open_new_tab(router)
+    exit()
+else:
+    if code == 200:
+        ###time.sleep(1)
+        print("路由器配置正常")
+        print()
+        print("正在检测SSH服务")
+        print()
+        time.sleep(1)
+        try:
+            zy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            zy.settimeout(3)
+            zy.connect(('192.168.99.1', 22))
+            zy.close()
+            print("SSH服务正常")
+            print()
+            shizuku()
+            exit(0)
+        except ConnectionRefusedError:
+            print("SSH服务异常")
+            print()
+            print("正在开启SSH服务")
+            print()
+            while True:
+                try:
+                    postData = "{'id': '1', 'jsonrpc': '2.0', 'method': 'call', 'params': ['00000000000000000000000000000000', 'xapi.basic', 'open_dropbear', {}]}"
+                    ubus = "http://192.168.99.1/ubus"
+                    response = requests.post(ubus, data=postData, timeout=4)
+                    break
+                except requests.exceptions.ConnectionError:
+                    print("SSH启动失败!"),print("请检查路由器状态"),print("回车退出")
+                    input(),time.sleep(3)
+                    exit()
+
+
+            #print(response.text)
+            print("SSH服务开启成功")
+            print()
+            shizuku()
+
+
+        #webbrowser.open(openssh, new=0, autoraise=True)
+        ###
+        #webbrowser.open_new_tab(openssh)
+
+
+
+
+
+
+
